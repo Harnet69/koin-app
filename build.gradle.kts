28df137782ee6513dc4c2f8497f6edf30eb8ba60
vital.kokhanau <harnet69@gmail.com>
2024-07-18 09:51:16 +0200

    Koin-app: Add Detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,20 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import io.gitlab.arturbosch.detekt.Detekt
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.detekt)
+}
+
+tasks.register<Detekt>("detektAll") {
+    description = "Detekt build"
+    buildUponDefaultConfig = true
+    setSource(file(projectDir))
+    config.setFrom(file("$rootDir/config/detekt/detekt.yml"))
+    include("**/*.kt")
+    autoCorrect = true
+}
+
+dependencies {
+    detektPlugins(libs.detekt.formatting)
 }
